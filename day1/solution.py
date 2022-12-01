e49b53586ee54cfd22823b9478bd49113195698a
Danny Edhouse <danny.edhouse@bjss.com>
2022-12-01 20:04:57 +0000

    add day 1

---
+++ day1/solution.py
@@ -0,0 +1,18 @@
+most_calories = {}
+
+with open('input.txt') as f:
+    elf_item_calories = f.read().splitlines()
+    calories = 0
+
+    for line in elf_item_calories:
+        if line:
+            calories += int(line)
+        else:
+            most_calories[len(most_calories)+1] = calories
+            calories=0
+
+print(max(most_calories.values()))
+print(max(most_calories, key=most_calories.get))
+
+
+        
